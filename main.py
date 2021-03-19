4e9eb311b96785a9ba267afd27d37b5280d0d22e
BrysonV10 <brysonvanryn@gmail.com>
2021-03-19 01:02:41 +0000

    Added a few more stats to the main html file. *To Do*: fix date code to add booted_at_time to be responsive and not static.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, jsonify
-import psutil
-
+import psutil, datetime
+version = 0.3
 app = Flask(__name__, static_folder="static")
-
+booted_at_time = "March 18, 8:52pm"
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', booted_at=booted_at_time)
 
 @app.route("/cpu")
 def cpu():
   cpu = psutil.cpu_percent(1)
   print(cpu)
-  return jsonify(cpu=str(cpu))
+  return jsonify(cpu=str(cpu), version=version)
+
+
 app.run(host='0.0.0.0')
