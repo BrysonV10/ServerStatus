c7e4ae63cb400bb6dc427e0b6922a8b7fa34c9a5
BrysonV10 <73202878+BrysonV10@users.noreply.github.com>
2021-12-22 20:57:23 -0500

    Add MC info and other general improvements

--- frontend/statusfrontend/src/components/disk-card.js
+++ frontend/statusfrontend/src/components/disk-card.js
@@ -2,6 +2,39 @@ import React from "react"
 import {Box, CircularProgress, Typography, Grid, Card, CardContent} from "@material-ui/core"
 import "../Styles.css"
 
+/**
+ * Format bytes as human-readable text.
+ * 
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * 
+ * @return Formatted string.
+ */
+function humanFileSize(bytes, si=false, dp=1) {
+    const thresh = si ? 1000 : 1024;
+  
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+  
+    const units = si 
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+  
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  
+  
+    return bytes.toFixed(dp) + ' ' + units[u];
+  }
+
+
 function CircularProgressWithLabel(props) {
     return (
       <Box position="relative" display="inline-flex">
@@ -38,12 +71,12 @@ class Disk extends React.Component {
         }
     }
     componentDidUpdate(prevProps, prevState){
-      if(prevProps.val != this.props.val){
+      if(prevProps.val !== this.props.val){
         this.setState({
           percent: (this.props.val.used / this.props.val.total) * 100
         })
       }
-      if(this.props.val != prevProps.val){
+      if(this.props.val !== prevProps.val){
         if(this.state.percent >=75){
           this.setState({
             color: "red-card"
@@ -64,15 +97,18 @@ class Disk extends React.Component {
       }
     }
     render(){
+        let niceTotal = "Total Space: " + humanFileSize(this.props.val.total);
         return (
         <Grid item >
                 <Card variant="outlined"  >
                 <CardContent className={this.state.color}>
+                <center>
                     <Typography variant="h5">
                         {this.props.stat}
                     </Typography>
-                <center>
                     <CircularProgressWithLabel thickness={5} value={this.state.percent} color="inherit"/>
+                    <br/>
+                    <Typography variant="p" style={{fontFamily: "roboto"}}>{niceTotal}</Typography>
                 </center>
                 </CardContent>
                 </Card>
